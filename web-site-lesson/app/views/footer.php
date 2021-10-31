efd1ae3ee899f7816064d67dbfc8eb664346e861
Spr1gan <Littlep@ukr.net>
2021-11-01 00:48:07 +0200

    add link

--- web-site-lesson/app/views/footer.php
+++ web-site-lesson/app/views/footer.php
@@ -17,7 +17,7 @@
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="/prokhorenko-pavel" class="nav-link p-0 text-muted">prokhorenko page</a></li>
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
         </ul>
     </div>
