97ce7112f30eb4fe2e9591894ac73d56fa040f8c
nvv <unitmac@icloud.com>
2021-11-06 23:29:32 +0200

    homework # 2. Add Two Numbers

---
+++ homework/leetcode2/narchuk_v/ListNode.php
@@ -0,0 +1,13 @@
+<?php
+
+class ListNode
+{
+    public int $val;
+    public ListNode $next;
+
+    function __construct(int $val = 0, ListNode $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
