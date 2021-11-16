7d74aa4747424f4c1039aad2fff05fafea62b52b
Alexander Pekhota <alex@pdffiller.com>
2021-11-16 18:25:16 +0200

    day 17 test

--- guzzle-example.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__."/vendor/autoload.php";
-
-$client = new \GuzzleHttp\Client();
-$response = $client->get("http://google.com");
-echo $response->getBody()->getContents();
-//var_dump();
